315aad01c5511da57ec0f1feaa31d148b35a51dd
James Couch <timothy.james.couch@gmail.com>
2025-09-07 16:22:13 -0600

    Add Org formula for Homebrew

---
+++ Formula/org.rb
@@ -0,0 +1,24 @@
+class Org < Formula
+  desc      "org: safe VM-backed dev helper"
+  homepage  "https://github.com/tjamescouch/org"
+  license   "MIT"
+  version   "0.1.0"
+
+  url "https://github.com/tjamescouch/org/releases/download/v0.1.0/orgctl-0.1.0.tar.gz"
+  sha256 "<<<paste sha256 here>>>"
+
+  depends_on "bash"
+  depends_on "coreutils"
+  depends_on "curl"
+
+  def install
+    libexec.install "orgctl"
+    (bin/"orgctl").write_env_script libexec/"orgctl", {
+      PATH: "#{Formula["bash"].opt_bin}:#{Formula["coreutils"].opt_bin}:#{HOMEBREW_PREFIX}/bin:#{ENV["PATH"]}",
+    }
+  end
+
+  test do
+    system "#{bin}/orgctl", "version"
+  end
+end
