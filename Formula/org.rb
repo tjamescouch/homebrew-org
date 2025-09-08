aadef2f3a407d0b79ea0f7751ff4b2fc07eaedf1
James Couch <timothy.james.couch@gmail.com>
2025-09-07 23:38:24 -0600

    Fix URL format for orgctl version 0.6.5

--- Formula/org.rb
+++ Formula/org.rb
@@ -4,7 +4,7 @@ class Org < Formula
   license   "MIT"
   version   "0.6.5"
 
-  url "https://github.com/tjamescouch/org/releases/download/v0.6.5/orgctl-0.6.5.tar.gz"
+  url "https://github.com/tjamescouch/org/releases/download/0.6.5/orgctl-v0.6.5.tar.gz"
   sha256 "3c431d10e904344d71b98242268e9bbd276f426fdb510771e45531d97dbd80a3"
 
   depends_on "bash"
