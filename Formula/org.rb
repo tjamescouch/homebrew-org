dea0be7eed869e5c68d0c30667f27524201cd0d1
James Couch <timothy.james.couch@gmail.com>
2025-09-07 21:44:22 -0600

    Update org formula to version 0.6.3

--- Formula/org.rb
+++ Formula/org.rb
@@ -2,10 +2,10 @@ class Org < Formula
   desc      "org: safe VM-backed dev helper"
   homepage  "https://github.com/tjamescouch/org"
   license   "MIT"
-  version   "0.4.0"
+  version   "0.6.3"
 
-  url "https://github.com/tjamescouch/org/releases/download/v0.4.0/orgctl-0.4.0.tar.gz"
-  sha256 "bc5747fbf8a1c35c4c5849488a5c7f84322c7df8c4aa46da0c0f5ab2bc1e059a"
+  url "https://github.com/tjamescouch/org/releases/download/v0.6.3/orgctl-0.6.3.tar.gz"
+  sha256 "44e5d05b441f2fd2093a773b56c3f457d52714bb06d2a2b544171f0067538a84"
 
   depends_on "bash"
   depends_on "coreutils"
