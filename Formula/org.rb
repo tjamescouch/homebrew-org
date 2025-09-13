bb2cc0b0356110d7a76ea9f177a61162759f3b24
James Couch <timothy.james.couch@gmail.com>
2025-09-13 15:31:56 -0600

    Bump version from 1.0.8 to 1.0.9

--- Formula/org.rb
+++ Formula/org.rb
@@ -2,10 +2,10 @@ class Org < Formula
   desc      "org: safe VM-backed dev helper"
   homepage  "https://github.com/tjamescouch/org"
   license   "MIT"
-  version   "1.0.8"
+  version   "1.0.9"
 
-  url "https://github.com/tjamescouch/org/releases/download/v1.0.8/orgctl-1.0.8.tar.gz"
-  sha256 "1b1291fb962a1d30a977ddcd5e36c0167895163447a868a75f3684b7f86572b7"
+  url "https://github.com/tjamescouch/org/releases/download/v1.0.9/orgctl-1.0.9.tar.gz"
+  sha256 "a5f7c50bd2c3ba50da9a49ecaf5258cc5bec37a6cab068489f10c7f287c83ce9"
 
   depends_on "bash"
   depends_on "coreutils"
